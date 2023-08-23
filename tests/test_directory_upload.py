7899192c758ba1a6b62f513faafaa162c8a84caa
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-08-24 01:43:50 +0200

    testing: directory upload (3)
    start writing the "check directory" function

--- tests/test_directory_upload.py
+++ tests/test_directory_upload.py
@@ -1,9 +1,19 @@
 import os
 import shutil
-from typing import Generator
+from typing import Generator, Literal
 import pytest
 import circadian_scp_upload
 from . import utils
+
+
+"""
+NEXT STEPS:
+
+* future dates will still be present locally
+* future dates will not be present remotely
+* check whether all lock files have been removed
+
+"""
 
 
 @pytest.fixture
@@ -13,13 +23,36 @@ def provide_test_directory() -> (
     tmp_dir_path, dummy_files = utils.provide_test_directory("directories")
     yield tmp_dir_path, dummy_files
     if os.path.exists(tmp_dir_path):
-        pass  # shutil.rmtree(tmp_dir_path)
+        # shutil.rmtree(tmp_dir_path)
+        pass
+
+
+def _check_directory_state(
+    path: str,
+    dummy_files: dict[str, dict[str, str]],
+    state: Literal["empty", "complete"],
+) -> None:
+    assert os.path.isdir(path)
+    for date_string in dummy_files.keys():
+        if state == "empty":
+            assert not os.path.exists(os.path.join(path, date_string))
+        else:
+            assert os.path.isdir(os.path.join(path, date_string))
+            for file_name, file_contents in dummy_files[date_string].items():
+                assert os.path.isfile(os.path.join(path, date_string, file_name))
+                with open(os.path.join(path, date_string, file_name), "r") as f:
+                    assert f.read() == file_contents
 
 
 def test_directory_upload(
     provide_test_directory: tuple[str, dict[str, dict[str, str]]]
 ) -> None:
     tmp_dir_path, dummy_files = provide_test_directory
+
+    print("tmp_dir_path =", tmp_dir_path)
+    print("dummy_files =", dummy_files)
+
+    _check_directory_state(tmp_dir_path, dummy_files, "complete")
 
     with circadian_scp_upload.RemoteConnection(
         *utils.load_credentials()
@@ -36,9 +69,14 @@ def test_directory_upload(
             variant="directories",
         ).run()
 
-    # TODO: download remote directory
+        _check_directory_state(tmp_dir_path, dummy_files, "empty")
 
-    # TODO: check equality locally
+        os.rmdir(tmp_dir_path)
 
-    # TODO: assert that local lock file does not exists
-    # TODO: assert that remote lock file does not exist
+        # download remote directory and check completeness
+        remote_connection.transfer_process.get(tmp_dir_path, tmp_dir_path)
+        _check_directory_state(tmp_dir_path, dummy_files, "complete")
+
+        # TODO: assert that remote lock file does not exist
+
+        remote_connection.connection.run(f"rm -rf {tmp_dir_path}")
