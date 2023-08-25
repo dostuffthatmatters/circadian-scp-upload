014a7ddced4ab75da1ef0be8daec6f78c5a9f14b
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-08-26 00:35:28 +0200

    #3 (6)
    Refactor `__init__.py`

--- circadian_scp_upload/__init__.py
+++ circadian_scp_upload/__init__.py
@@ -1,3 +1,3 @@
+from . import utils, checksum
 from .utils import UploadClientCallbacks
 from .client import RemoteConnection, DailyTransferClient
-from .checksum import get_dir_checksum, get_file_checksum
